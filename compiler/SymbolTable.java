ef03e055d13a441a138bb0858c35e958a2846e2f
reynoldsbrent <brentsreynolds5@gmail.com>
2024-03-31 13:01:22 -0400

    Added Symbol Table structure

--- compiler/SymbolTable.java
+++ compiler/SymbolTable.java
@@ -1,5 +1,20 @@
 package compiler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SymbolTable {
-    
+    private final Map<String, String> variables = new HashMap<>();
+
+    public void register(String name, String type) {
+        variables.put(name, type);
+    }
+
+    public String getType(String name) {
+        return variables.get(name);
+    }
+
+    public boolean isDeclared(String name) {
+        return variables.containsKey(name);
+    }
 }
