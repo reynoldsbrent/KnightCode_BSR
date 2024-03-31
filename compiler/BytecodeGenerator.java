7062e19a95ea9952c8d5341bd7d0e65f414a0e71
reynoldsbrent <brentsreynolds5@gmail.com>
2024-03-31 12:59:11 -0400

    Added method structure for generating Bytecode

--- compiler/BytecodeGenerator.java
+++ compiler/BytecodeGenerator.java
@@ -1,5 +1,42 @@
 package compiler;
 
-public class BytecodeGenerator {
-    
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+public class BytecodeGenerator implements Opcodes {
+    private ClassWriter classWriter;
+    private MethodVisitor methodVisitor;
+
+    public BytecodeGenerator() {
+        classWriter = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+        // Start a class definition
+        classWriter.visit(V1_8, ACC_PUBLIC, "KnightCodeProgram", null, "java/lang/Object", null);
+        // Initialize the default constructor
+        initConstructor();
+    }
+
+    private void initConstructor() {
+        // Implement the constructor for the class
+    }
+
+    public void startMainMethod() {
+        // Begin the main method
+    }
+
+    public void endMainMethod() {
+        // End the main method
+    }
+
+    public byte[] getBytecode() {
+        return classWriter.toByteArray();
+    }
+
+    // Method for adding an integer variable
+    public void addIntegerVariable(String name, int value) {
+        // Use methodVisitor to visitVarInsn and visitIntInsn
+    }
+
+    // Additional methods for handling arithmetic, control structures, etc.
 }
+
