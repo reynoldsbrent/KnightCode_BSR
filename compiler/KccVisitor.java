a8a1e8e5feee1c5029d539693a6c85627ae8c9ac
reynoldsbrent <brentsreynolds5@gmail.com>
2024-03-31 13:00:34 -0400

    Added additional methods for visitor pattern

--- compiler/KccVisitor.java
+++ compiler/KccVisitor.java
@@ -1,7 +1,29 @@
 package compiler;
 
 import lexparse.KnightCodeBaseVisitor;
+import lexparse.KnightCodeParser;
 
-public class KccVisitor extends KnightCodeBaseVisitor{
-    
+public class KccVisitor extends KnightCodeBaseVisitor<Void> {
+    private SymbolTable symbolTable = new SymbolTable();
+    private BytecodeGenerator bytecodeGenerator = new BytecodeGenerator();
+
+    @Override
+    public Void visitDeclare(KnightCodeParser.DeclareContext ctx) {
+        for (KnightCodeParser.VariableContext variableCtx : ctx.variable()) {
+        String varName = variableCtx.identifier().getText();
+        String varType = variableCtx.vartype().getText();
+        symbolTable.register(varName, varType);
+        }
+        return null;
+    }
+
+
+    @Override
+    public Void visitPrint(KnightCodeParser.PrintContext ctx) {
+        // Handle print statements, including bytecode generation
+        return null;
+    }
+
+    // Implement additional visit methods for arithmetic, control structures, etc.
 }
+
