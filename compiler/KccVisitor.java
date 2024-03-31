1a68a879edc227b1bcfc8cbdd85abb9ac26a4351
reynoldsbrent <brentsreynolds5@gmail.com>
2024-03-31 16:33:58 -0400

    Updated visitDeclare method

--- compiler/KccVisitor.java
+++ compiler/KccVisitor.java
@@ -10,9 +10,11 @@ public class KccVisitor extends KnightCodeBaseVisitor<Void> {
     @Override
     public Void visitDeclare(KnightCodeParser.DeclareContext ctx) {
         for (KnightCodeParser.VariableContext variableCtx : ctx.variable()) {
-        String varName = variableCtx.identifier().getText();
-        String varType = variableCtx.vartype().getText();
-        symbolTable.register(varName, varType);
+            String varName = variableCtx.identifier().getText();
+            String varType = variableCtx.vartype().getText();
+            symbolTable.register(varName, varType);
+            // Example: Assuming all variables are integers for simplicity
+            bytecodeGenerator.addIntegerVariable(varName, 0); // Initializing with 0
         }
         return null;
     }
@@ -22,6 +24,10 @@ public class KccVisitor extends KnightCodeBaseVisitor<Void> {
     public Void visitPrint(KnightCodeParser.PrintContext ctx) {
         // Handle print statements, including bytecode generation
         return null;
+    }
+
+    public BytecodeGenerator getBytecodeGenerator() {
+        return this.bytecodeGenerator;
     }
 
     // Implement additional visit methods for arithmetic, control structures, etc.
