0943843b739280628fe34ca33271c76acb930b36
reynoldsbrent <brentsreynolds5@gmail.com>
2024-04-01 23:01:17 -0400

    Can print a string

--- compiler/SymbolTable.java
+++ compiler/SymbolTable.java
@@ -4,16 +4,40 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SymbolTable {
-    private final Map<String, String> variables = new HashMap<>();
+    private final Map<String, VariableInfo> variables = new HashMap<>();
+    private int nextIndex = 0; // Tracks the next available index for local variables
 
+    // Inner class to hold variable information (type and index)
+    private static class VariableInfo {
+        String type;
+        int index;
+
+        VariableInfo(String type, int index) {
+            this.type = type;
+            this.index = index;
+        }
+    }
+
+    // Registers a new variable with its type. Assigns the next available index to it.
     public void register(String name, String type) {
-        variables.put(name, type);
+        if (!variables.containsKey(name)) {
+            variables.put(name, new VariableInfo(type, nextIndex++));
+        }
     }
 
+    // Retrieves the type of a variable by name
     public String getType(String name) {
-        return variables.get(name);
+        VariableInfo info = variables.get(name);
+        return (info != null) ? info.type : null;
     }
 
+    // Retrieves the index of a variable by name
+    public int getIndex(String name) {
+        VariableInfo info = variables.get(name);
+        return (info != null) ? info.index : -1;
+    }
+
+    // Checks if a variable is declared
     public boolean isDeclared(String name) {
         return variables.containsKey(name);
     }
